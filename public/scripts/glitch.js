2f2ebf18260a8c8475da711af3b885c715428ce1
realsumit <sumitsarinofficial@gmail.com>
2017-10-05 12:45:22 +0530

    Initial Dev

--- public/scripts/glitch.js
+++ public/scripts/glitch.js
@@ -1,14 +1,15 @@
-var text = document.getElementsByClassName('glitch')[0];
+document.getElementsByClassName('glitch').forEach(text){
 
-setInterval(myMethod, 140);
-var i=0;
+	setInterval(myMethod, 140);
+	var i=0;
 
-function myMethod(){
-	var string = 'Code. Compile. Run.';
-	text.innerHTML = string.substring(0,i) + '/';
-	i++;
-	if (i==string.length+1){
-		setTimeout(function myMethod2(){}, 10000);
-		i--;
+	function myMethod(){
+		var string = 'Code. Compile. Run.';
+		text.innerHTML = string.substring(0,i) + '/';
+		i++;
+		if (i==string.length+1){
+			setTimeout(function myMethod2(){}, 10000);
+			i--;
+		}
 	}
-}
+};
