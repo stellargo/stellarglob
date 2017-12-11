fb104f2d24a5807a262b7543f4846949b8c4cfdd
realsumit <sumitsarinofficial@gmail.com>
2017-12-11 20:32:34 +0530

    regular

--- index.js
+++ index.js
@@ -56,6 +56,9 @@ io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
+  socket.on('disconnect', function(){
+    io.emit('chat message', 'user has disconnected');
+  });
 });
 
 //********************************************************
