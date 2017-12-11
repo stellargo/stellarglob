479d3903d83bbd915ab66d39f19d4c9bfa98d913
realsumit <sumitsarinofficial@gmail.com>
2017-12-11 15:38:12 +0530

    regular

--- index.js
+++ index.js
@@ -34,9 +34,8 @@ app.get("/stellarMsg",function(req,res){
 });
 
 io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
   });
 });
 
