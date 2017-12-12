a2294efb2776f7122883407618ee592592880eb9
realsumit <sumitsarinofficial@gmail.com>
2017-12-12 17:02:44 +0530

    regular

--- index.js
+++ index.js
@@ -20,7 +20,9 @@ var serveIndex = require('serve-index');
 
 app.use(express.static(__dirname + "/stellarnotes"));
 app.use('/stellarnotes', serveIndex(__dirname + '/stellarnotes'));
-app.get("/*.pdf",function(req,res){
+app.use(express.static(__dirname + "/stellarnotes1"));
+app.use('/stellarnotes1', serveIndex(__dirname + '/stellarnotes1'));
+app.get("/stellarnotes/*.pdf",function(req,res){
 	console.log(req.params[0]);
 	res.render("stellarnote",{ note: req.params[0] });
 });
