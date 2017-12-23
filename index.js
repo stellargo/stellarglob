ba4f5048d587fa6279dcfa5695bc22ff971ec84c
realsumit <sumitsarinofficial@gmail.com>
2017-12-23 21:48:46 +0530

    updates

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ var serveIndex = require('serve-index');
 app.use(express.static(__dirname + "/public/images"));
 app.use('/images', serveIndex(__dirname + '/public/images'));
 app.use(express.static(__dirname + "/public/notes"));
-app.use('/stellarnotes', serveIndex(__dirname + '/public/notes'));
+app.use('/notes', serveIndex(__dirname + '/public/notes'));
 // app.get("/stellarnotes/*.pdf",function(req,res){
 // 	console.log(req.params[0]);
 // 	res.render("stellarnote",{ note: req.params[0] });
