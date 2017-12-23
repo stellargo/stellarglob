d9ec624df7f5dfa9b6f3dd00255bf090e60f332a
realsumit <sumitsarinofficial@gmail.com>
2017-12-23 16:24:50 +0530

    updated

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ var express 	= require("express"),
 
 var serveIndex = require('serve-index');
 
+app.use(express.static(__dirname + "/public/images"));
 app.use(express.static(__dirname + "/stellarnotes"));
 app.use('/stellarnotes', serveIndex(__dirname + '/stellarnotes'));
 app.use(express.static(__dirname + "/stellarnotes1"));
